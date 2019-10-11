9e0e4a655c09369d8b27978339511bdcf000e405
lixiaoxu <lixiaoxu@askingdata.com>
2019-10-12 01:39:26 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/OpenCVDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCVDemo.dir/main.cpp.o"
+  "OpenCVDemo.pdb"
+  "OpenCVDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCVDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
